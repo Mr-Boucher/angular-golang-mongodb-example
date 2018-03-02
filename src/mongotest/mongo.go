8cc2258ac1835054dd17d24f15e167d44418621c
Mr-Boucher <bane99!!>
2018-03-01 20:06:48 -0400

    refactored to create framework

--- src/mongotest/mongo.go
+++ src/mongotest/mongo.go
@@ -16,22 +16,22 @@ import (
 
 //Test data format
 type TestData struct {
-	ObjectId bson.ObjectId   `bson:"_id,omitempty" json:"-"` //Setup mapping for data from bson(used for Mongo) and json(Used by REST API response) note the "-" it means that json does not have this
-	Value    string   `bson:"value" json:"value"`            //Setup mapping for data from bson(used for Mongo) and json(Used by REST API response)
-	Id       string   `bson:"id" json:"id"`                  //Setup mapping for data from bson(used for Mongo) and json(Used by REST API response)
+	ObjectId bson.ObjectId `bson:"_id,omitempty" json:"-"` //Setup mapping for data from bson(used for Mongo) and json(Used by REST API response) note the "-" it means that json does not have this
+	Value    string        `bson:"value" json:"value"`     //Setup mapping for data from bson(used for Mongo) and json(Used by REST API response)
+	Id       string        `bson:"id" json:"id"`           //Setup mapping for data from bson(used for Mongo) and json(Used by REST API response)
 }
 
 type MongoDB struct {
-	databaseName string
+	databaseName   string
 	collectionName string
-	cluster []string
-	userDatabase string
-	username string
-	password string
+	cluster        []string
+	userDatabase   string
+	username       string
+	password       string
 }
 
 //Should be a constant but can't because of language restriction that const can't have arrays
-var mongoDB = MongoDB{ "dev", "test", []string{"cluster0-shard-00-00-iaz9w.mongodb.net:27017"}, "admin", "dev", "dev" }
+var mongoDB = MongoDB{"dev", "test", []string{"cluster0-shard-00-00-iaz9w.mongodb.net:27017"}, "admin", "dev", "dev"}
 
 //Kick it all off
 func main() {
@@ -50,24 +50,25 @@ func main() {
 
 	//Setup route for incoming data requests
 	router := mux.NewRouter()
-	router.HandleFunc("/data", options).Methods("OPTIONS") //Setup data the REST API and call options
+	router.HandleFunc("/data", options).Methods("OPTIONS")             //Setup data the REST API and call options
 	router.HandleFunc("/data/{id:[0-9]+}", options).Methods("OPTIONS") //Setup data the REST API and options
 
-	router.HandleFunc("/data", getData).Methods("GET") //Setup data as the REST API and call GetData for get requests
-	router.HandleFunc("/data", createData).Methods("POST") //Setup data the REST API and call CreateData for delete requests
-	router.HandleFunc("/data/{id:[0-9]+}", updateData).Methods("PUT") //Setup data the REST API and call UpdateData for delete requests
+	router.HandleFunc("/data", getData).Methods("GET")                   //Setup data as the REST API and call GetData for get requests
+	router.HandleFunc("/data", createData).Methods("POST")               //Setup data the REST API and call CreateData for delete requests
+	router.HandleFunc("/data/{id:[0-9]+}", updateData).Methods("PUT")    //Setup data the REST API and call UpdateData for delete requests
 	router.HandleFunc("/data/{id:[0-9]+}", deleteData).Methods("DELETE") //Setup data the REST API and call DeleteData for delete requests
 
 	//Start listening for requests - thread waits forever at this port
-	log.Fatal(http.ListenAndServe(":" + port, router))
+	log.Fatal(http.ListenAndServe(":"+port, router))
 }
 
+////////////////////////////////////////REST API FUNCTIONS/////////////////////////////////////////////////////////////////////////
 //Set headers to tell the client what is supported for this REST API
 func setHeaders(writer http.ResponseWriter) {
-	writer.Header().Add("Access-Control-Allow-Origin", "*") //Allow access from anywhere
+	writer.Header().Add("Access-Control-Allow-Origin", "*")                                            //Allow access from anywhere
 	writer.Header().Add("Access-Control-Allow-Headers", "Content-Type, Access-Control-Request-Origin") //Allows setting of the Content-Type by the client
-	writer.Header().Add("Access-Control-Allow-Methods", "HEAD, GET, POST, PUT, DELETE, OPTIONS") //REST API supports GET, POST, PUT, DELETE
-	writer.Header().Add("Accept", "application/json") //Only json is accepted
+	writer.Header().Add("Access-Control-Allow-Methods", "HEAD, GET, POST, PUT, DELETE, OPTIONS")       //REST API supports GET, POST, PUT, DELETE
+	writer.Header().Add("Accept", "application/json")                                                  //Only json is accepted
 	writer.Header().Add("Content-Type", "application/json")
 }
 
@@ -75,41 +76,114 @@ func setHeaders(writer http.ResponseWriter) {
 //As well as set up security headers
 func options(writer http.ResponseWriter, request *http.Request) {
 	fmt.Println("options")
-	setHeaders( writer )
+	setHeaders(writer)
 }
 
 //Get the data from mongo
 func getData(writer http.ResponseWriter, request *http.Request) {
 	fmt.Println("getData:", request.URL.Path)
-	setHeaders( writer ) //Set response headers
-	data := loadData(mongoDB) //load the data
+	setHeaders(writer) //Set response headers
+
+	data := execute(mongoDB, load, nil)
+
 	json.NewEncoder(writer).Encode(data) //stream the encoded data on the writer
 }
 
+//
 func createData(writer http.ResponseWriter, request *http.Request) {
 	fmt.Println("createData:", request.URL.Path)
-	setHeaders( writer ) //Set response headers
+	setHeaders(writer) //Set response headers
+	execute(mongoDB, create, nil)
 }
 
+//
 func updateData(writer http.ResponseWriter, request *http.Request) {
 	fmt.Println("updateData:", request.URL.Path)
-	setHeaders( writer ) //Set response headers
+	setHeaders(writer) //Set response headers
+	execute(mongoDB, update, nil)
 }
 
+//
 func deleteData(writer http.ResponseWriter, request *http.Request) {
 	params := mux.Vars(request)
-	fmt.Printf("deleteData(%s):%s\n", params["id"], request.URL.Path)
-	setHeaders( writer ) //Set response headers
+	id := params["id"]
+	fmt.Printf("deleteData(%s):%s\n", id, request.URL.Path)
+	setHeaders(writer) //Set response headers
 
+	//perform the deleteDataById
+	//args: actionArguments = id
+	execute(mongoDB, deleteById, nil)
 }
 
-
+////////////////////////////////////////ACTION FUNCTIONS/////////////////////////////////////////////////////////////////////////
 //Load data from mongo
-func loadData( databaseConnectionInfo MongoDB ) []TestData {
+func load(collection *mgo.Collection, arguments actionArguments) []TestData {
+
+	//Load data
+	query := collection.Find(bson.M{})
+	query = query.Sort("value") //sort the data by its value
+	var results []TestData
+	query.All(&results) //execute the query
+
+	//Display the data returned for debugging
+	for index, result := range results {
+		fmt.Println(index, "id:", result.Id, "value:", result.Value)
+	}
+
+	fmt.Println("Finished loading data")
+
+	return results
+}
+
+//remove data from db base
+func create(collection *mgo.Collection, arguments actionArguments) []TestData {
+
+	fmt.Println("create:", "started")
+	//collection.Remove( bson.M{"id": id} )
+	fmt.Println("create:", "finished")
+	return nil
+}
+
+//remove data from db base
+func update(collection *mgo.Collection, arguments actionArguments) []TestData {
+
+	fmt.Println("update:", "started")
+	//collection.Remove( bson.M{"id": id} )
+	fmt.Println("update:", "finished")
+	return nil
+}
+
+//remove data from db base
+func deleteById(collection *mgo.Collection, arguments actionArguments) []TestData {
+
+	if arguments == nil {
+		panic( "Missing arguments" )
+	}
+
+	id := arguments.getId
+	if id == nil {
+		panic( "Arguments missing id" )
+	}
+
+	fmt.Println("deleteById:", id, "started")
+	//collection.Remove( bson.M{"id": id} )
+	fmt.Println("deleteById:", id, "finished")
+	return nil
+}
+
+////////////////////////////////////////FRAMEWORK/////////////////////////////////////////////////////////////////////////
+//arguments for different actions
+type actionArguments interface {
+	getId() string
+}
+
+//Method handling framework calls to mongoDB this method will create and destroy all resources needed
+//to work with mongoDB it will perform the action function and return the results
+func execute(databaseConnectionInfo MongoDB, action func(collection *mgo.Collection, args actionArguments) []TestData, arguments actionArguments) []TestData {
 
 	//set connection to mongo
 	dialInfo := &mgo.DialInfo{
-		Addrs: databaseConnectionInfo.cluster,
+		Addrs:    databaseConnectionInfo.cluster,
 		Database: databaseConnectionInfo.userDatabase,
 		Username: databaseConnectionInfo.username,
 		Password: databaseConnectionInfo.password,
@@ -132,7 +206,10 @@ func loadData( databaseConnectionInfo MongoDB ) []TestData {
 	session, err := mgo.DialWithInfo(dialInfo)
 	if err != nil {
 		panic(err)
-	} //fail in null
+	}
+
+	//If we have a valid session make sure it is closed once the method exits otherwise it will be a session leak
+	defer session.Close();
 
 	//Get the mongo db from the session
 	fmt.Println("Opening DB", databaseConnectionInfo.databaseName, "using session", session)
@@ -147,23 +224,8 @@ func loadData( databaseConnectionInfo MongoDB ) []TestData {
 		panic(collection)
 	}
 
-	//Load data
-	query := collection.Find(bson.M{})
-	query = query.Sort("value")
-	if err != nil {
-		panic(err)
-	}
+	//execute the acton function
+	result := action(collection, arguments)
 
-	//
-	//fmt.Println( "Count:", count)
-	results := []TestData{}
-	query.All(&results)
-	for index, result := range results {
-		fmt.Println(index, "id:", result.Id)
-		fmt.Println("value:", result.Value)
-	}
-
-	fmt.Println("Finished loading data")
-
-	return results
+	return result;
 }
