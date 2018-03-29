30c216e364a51f2a950912cf7c70a6f3e5e1f807
cboucher <cboucher@casenetllc.com>
2018-03-29 07:01:00 -0400

    Added Client side paging

--- client/angular-client/src/app/app.module.ts
+++ client/angular-client/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 
 import { AppComponent } from './app.component';
@@ -26,7 +27,8 @@ import {ConfigurationService} from "./configuration/configuration.service";
     BrowserModule,
     HttpClientModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxPaginationModule
   ],
   providers: [
     HttpService,
